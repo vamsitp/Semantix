fca4918a87cb75905635b28a9de3170910e6c422
Vamsi TP <vamsitp@microsoft.com>
2021-02-20 15:27:26 +0530

    Add project files.

---
+++ Semantix/FileService.cs
@@ -0,0 +1,60 @@
+﻿namespace Semantix
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    using CsvHelper;
+    using CsvHelper.Configuration;
+    using CsvHelper.Excel;
+
+    public class FileService
+    {
+        public IEnumerable<T> GetRecords<T>(string file, bool isExcel = false, string sheetName = null)
+        {
+            if (isExcel)
+            {
+                var excelParser = new ExcelParser(file, sheetName, new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    PrepareHeaderForMatch = (header, i) => header.SanitizeHeader()
+                });
+
+                using (var csvReader = new CsvReader(excelParser))
+                {
+                    var results = csvReader.GetRecords<T>().ToList();
+                    return results;
+                }
+            }
+            else
+            {
+                var textReader = new StreamReader(file);
+                using (var csvReader = new CsvReader(textReader, new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    PrepareHeaderForMatch = (header, i) => header.SanitizeHeader()
+                }))
+                {
+                    var results = csvReader.GetRecords<T>().ToList();
+                    return results;
+                }
+            }
+        }
+
+        public void WriteRecords<T>(IEnumerable<T> records, string outputPath)
+        {
+            using (var excelWriter = new ExcelWriter(outputPath, CultureInfo.InvariantCulture))
+            {
+                excelWriter.WriteRecords(records);
+            }
+        }
+    }
+
+    public static class SemantixExtensions
+    {
+        public static string SanitizeHeader(this string header)
+        {
+            return Regex.Replace(header, @"(\s+|@|&|'|\(|\)|<|>|#|\?|\.|""|\-)", string.Empty);
+        }
+    }
+}
---
+++ Semantix/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Semantix
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using SimMetrics.Net.Metric;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = args[0];
+            if (File.Exists(file))
+            {
+                var fileService = new FileService();
+                var list1 = fileService.GetRecords<dynamic>(file, true, args[1])?.ToList();
+                var list2 = fileService.GetRecords<dynamic>(file, true, args[2])?.ToList();
+
+                var titleComparisons = new List<(string Title1, string Title2, double Similarity)>();
+                var s = new SmithWatermanGotoh(); // new SmithWatermanGotoh(new AffineGapRange5To0Multiplier1(), new SubCostRange5ToMinus3());
+                Console.WriteLine($"list1: {list1?.Count ?? -1} | list2: {list2?.Count}");
+                foreach (var t1 in list1)
+                {
+                    var t2 = list2.Select(l2 => new { Title = l2.Title, Similarity = s.GetSimilarity(t1.Title, l2.Title) }).OrderByDescending(l => l.Similarity).FirstOrDefault();
+                    var titleComparison = $"{t1.Title} | {t2.Title} | {t2.Similarity}";
+                    titleComparisons.Add((t1.Title, t2.Title, t2.Similarity));
+                    Console.WriteLine(titleComparison);
+                }
+
+                fileService.WriteRecords(titleComparisons, "Comparison.xlsx");
+                Console.WriteLine("Done!");
+            }
+        }
+    }
+}
